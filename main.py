9136de2669a766c0795d1b78b4f0e848c9ba9d20
Simon Champney <simonhchampney@gmail.com>
2023-12-08 17:32:23 +0000

    get all

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-imoort os, time, robin_stocks.robinhood
-
-robin_stocks.robinhood.authentication.login(username=Simon111, password=getpass("Password: "))
-print(robin_stocks.robinhood.stocks.get_ratings("AAPL"))
+import os, time, robin_stocks.robinhood, getpass
+getpass=getpass.getpass
+try:
+    robin_stocks.robinhood.authentication.login()
+except:
+    robin_stocks.robinhood.authentication.login(username="Simonhchampney@gmail.com", password=getpass("Password: "))
+print(robin_stocks.robinhood.stocks.get_ratings("AAPL", info="summary"))
