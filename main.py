a73e8ebffac24d9e1feb526d270882e365e1cf11
Simon Champney <simonhchampney@gmail.com>
2023-12-08 17:10:04 +0000

    added test now

---
+++ main.py
@@ -0,0 +1,4 @@
+imoort os, time, robin_stocks.robinhood
+
+robin_stocks.robinhood.authentication.login(username=Simon111, password=getpass("Password: "))
+print(robin_stocks.robinhood.stocks.get_ratings("AAPL"))
