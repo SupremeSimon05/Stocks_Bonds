948aa94bd89ba86f31d4a8fa89359dcaadad14ba
Simon Champney <simonhchampney@gmail.com>
2023-12-09 05:23:24 +0000

    Made some code, should maybe work???

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-import os, time, robin_stocks.robinhood, getpass
+import os, time, robin_stocks.robinhood, getpass, string, random
 getpass=getpass.getpass
-try:
-    robin_stocks.robinhood.authentication.login()
-except:
-    robin_stocks.robinhood.authentication.login(username="Simonhchampney@gmail.com", password=getpass("Password: "))
-print(robin_stocks.robinhood.stocks.get_ratings("AAPL", info="summary"))
+def login(email="Simonhchampney@gmail.com"):
+    if(email=="Simonhchampney@gmail.com"):
+        try:
+            robin_stocks.robinhood.authentication.login()
+        except:
+            robin_stocks.robinhood.authentication.login(username=email, password=getpass("Password: "))
+    else:
+        robin_stocks.robinhood.authentication.login(username=email, password=getpass("Password: "))
+def query_symbols(amt):
+    templ = [] # temp set here
+    for i in range(amt):
+        temps="" # temp set here
+        for j in range(random.randint(1,4)):
+            temps+=random.choice(string.ascii_uppercase)
+        tempo=robin_stocks.robinhood.stocks.get_stock_quote_by_symbol(temps) # temp set here
+        if(tempo):
+            templ=tempo
+    return templ
+
+login()
+robin_stocks.robinhood.stocks.get_ratings("AAPL", info="summary")
+print(query_symbols(200))
