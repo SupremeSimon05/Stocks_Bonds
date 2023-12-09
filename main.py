e90c30a1857b4268ee31ddaa7332879cd6f63530
Simon Champney <simonhchampney@gmail.com>
2023-12-09 10:22:40 +0000

    Oh my joe? does it work?

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ def login(email="Simonhchampney@gmail.com"):
 
 login()
 watchlist=robin_stocks.robinhood.account.get_watchlist_by_name("not")
-cash=robin_stocks.robinhood.account.load_account_profile()["cash"]
+account=robin_stocks.robinhood.account.load_account_profile()
+cash=float(robin_stocks.robinhood.profiles.load_account_profile()['portfolio_cash'])-\
+    sum(float(order['price']) * float(order['quantity']) 
+    for order in robin_stocks.robinhood.orders.get_all_open_stock_orders() 
+    if order['side'] == 'buy')
+
 if(cash>1):
     to_buys = [item['symbol'] for item in watchlist['results']]
     temp = []
@@ -56,16 +61,46 @@ if(cash>1):
     symbol_amt_to_buy={}
     for to_buy in to_buys:
         symbol_amt_to_buy[to_buy]=0
-    while(cash>=0):
+    while(cash>0):
+        if(not to_buys):
+            break
         for to_buy in to_buys:
             if(symbol_price[to_buy]<cash):
                 symbol_amt_to_buy[to_buy]+=1
+                cash-=symbol_price[to_buy]
             else:
                 cash=0
+    print(symbol_amt_to_buy)
     for symbol in symbol_amt_to_buy:
-        #place limit orders here
-            
+        order = robin_stocks.robinhood.orders.order(symbol, symbol_amt_to_buy[symbol], "buy", limitPrice=symbol_price[symbol]-0.01, timeInForce="gtc")
+        print(order)
+owned_to_how_much={}
+for symb, dumb in robin_stocks.robinhood.account.build_holdings().items():
+    quantity=int(float(dumb["quantity"]))
+    if(quantity>1):
+        owned_to_how_much[symb]=quantity
+to_sells=[]
+for to_sell in owned_to_how_much.keys():
+    summary = robin_stocks.robinhood.stocks.get_ratings(to_sell)["summary"]
+    total = summary["num_buy_ratings"]+summary["num_hold_ratings"]+summary["num_sell_ratings"]
+    if(summary["num_buy_ratings"]/total<=0.9):
+        to_sells.append(to_sell)
+temp=[]
+for to_sell in to_sells:
+    orders=robin_stocks.robinhood.orders.get_all_stock_orders()
+    for order in orders:
+        if(to_buy==order['instrument'].split('/')[-2]):
+            if(order["state"]=="filled"):
+                if((datetime.datetime.now()-datetime.datetime.strptime(order['last_transaction_at'], "%Y-%m-%dT%H:%M:%SZ")).total_seconds()/3600>24):
+                    temp.append(to_sell)
+            else:
+                temp.append(to_sell)
+to_sells = temp[:]
+for to_sell in to_sells:
+    order = robin_stocks.robinhood.orders.order(to_sell, owned_to_how_much[to_sell], "sell", limitPrice=owned_to_how_much[symbol]+0.02, timeInForce="gtc")
+    print(order)
 
 
-#print(robin_stocks.robinhood.stocks.get_ratings("LLAP")["summary"])
-#print(query_symbols(200))
+
+
+#"""
